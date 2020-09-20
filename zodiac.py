4b36afc20775caaab80175fd303f17e02a24e555
mohitgovindwar <71601625+mohitgovindwar@users.noreply.github.com>
2020-09-20 22:06:48 +0530

    Add files via upload

---
+++ zodiac.py
@@ -0,0 +1,16 @@
+print('''
+1]Aries
+2]Tauras
+3]Gemini
+4]Cancer
+5]Leo
+6]Virgo
+7]Libra
+8]Scorpio
+9]Sagittarius
+10]Capricorn
+11]Aquarius
+12]Pisces
+''')
+
+s = input("Pick your sign number and Press Enter")
